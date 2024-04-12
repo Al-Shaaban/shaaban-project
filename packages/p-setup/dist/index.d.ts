4dcea79b4bf333537a70dfba0fd5e608b79b1646
Abed Al Ghani Mosbah Shaaban <abedshaaban600@gmail.com>
2024-04-12 21:37:22 +0300

    refactor: removed dist folder

--- packages/p-setup/dist/index.d.ts
+++
@@ -1,2 +0,0 @@
-export * from "C:/Users/user/OneDrive/Documents/Abed/shaaban-org/shaaban-project/packages/p-setup/src/index";
-export { default } from "C:/Users/user/OneDrive/Documents/Abed/shaaban-org/shaaban-project/packages/p-setup/src/index";
