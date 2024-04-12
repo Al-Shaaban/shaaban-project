8ddab2ea769959907fc75b70838681b34d3d955e
Abed Al Ghani Mosbah Shaaban <abedshaaban600@gmail.com>
2024-04-12 23:25:01 +0300

    refactor: setup import alias

--- packages/p-setup/template-react-vite-ts-tw/vite.config.ts
+++ packages/p-setup/template-react-vite-ts-tw/vite.config.ts
@@ -1,7 +1,17 @@
 import react from "@vitejs/plugin-react-swc"
 import { defineConfig } from "vite"
+import { fileURLToPath } from "node:url"
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve:{
+    alias: [
+      {
+        find: '@',
+        replacement: fileURLToPath(new URL('./src', import.meta.url))
+      }
+    ]
+  }
 })
+ 
