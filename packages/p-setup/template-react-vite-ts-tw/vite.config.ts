eaa3cb29b081ecb9ddd9b1fa2efd66d2bfb0c037
Abed Al Ghani Mosbah Shaaban <abedshaaban600@gmail.com>
2024-04-12 23:36:25 +0300

    refactor: added important aliases

--- packages/p-setup/template-react-vite-ts-tw/vite.config.ts
+++ packages/p-setup/template-react-vite-ts-tw/vite.config.ts
@@ -1,11 +1,11 @@
-import react from "@vitejs/plugin-react-swc"
-import { defineConfig } from "vite"
-import { fileURLToPath } from "node:url"
+import { fileURLToPath } from 'node:url'
+import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  resolve:{
+  resolve: {
     alias: [
       {
         find: '@',
@@ -14,8 +14,19 @@ export default defineConfig({
       {
         find: '@styles',
         replacement: fileURLToPath(new URL('./src/lib/styles', import.meta.url))
+      },
+      {
+        find: '@hooks',
+        replacement: fileURLToPath(new URL('./src/lib/hooks', import.meta.url))
+      },
+      {
+        find: '@utils',
+        replacement: fileURLToPath(new URL('./src/lib/utils', import.meta.url))
+      },
+      {
+        find: '@types',
+        replacement: fileURLToPath(new URL('./src/lib/types', import.meta.url))
       }
     ]
   }
 })
- 
