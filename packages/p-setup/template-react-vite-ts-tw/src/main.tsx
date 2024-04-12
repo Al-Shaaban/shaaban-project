459ea90465b24fb66207a5b0ea7bada51fe12036
Abed Al Ghani Mosbah Shaaban <abedshaaban600@gmail.com>
2024-04-13 00:18:49 +0300

    refactor: added react query from tanstack

--- packages/p-setup/template-react-vite-ts-tw/src/main.tsx
+++ packages/p-setup/template-react-vite-ts-tw/src/main.tsx
@@ -1,4 +1,6 @@
 import React from 'react'
+import { QueryClientProvider } from '@tanstack/react-query'
+import { queryClient } from '@utils/index.ts'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 
@@ -8,8 +10,10 @@ import '@styles/globals.css'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </QueryClientProvider>
   </React.StrictMode>
 )
