1d44e52bcc88533901d3420318c830a39f52d6a8
Abed Al Ghani Mosbah Shaaban <abedshaaban600@gmail.com>
2024-04-13 00:02:41 +0300

    refactor: created paths constant for routes

---
+++ packages/p-setup/template-react-vite-ts-tw/src/lib/constants/paths.ts
@@ -0,0 +1,7 @@
+export const Paths = {
+  HOME_PAGE: '/',
+
+  AUTH_LOGIN_PAGE: '/auth/login',
+  AUTH_NEW_PASSWORD_PAGE: '/auth/new-password',
+  AUTH_RESET_PASSWORD_PAGE: '/auth/reset-password'
+}
