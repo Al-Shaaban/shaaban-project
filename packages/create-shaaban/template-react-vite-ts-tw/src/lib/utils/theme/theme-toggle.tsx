62716bd4282b1256d51e9ae46b2e4b06167f432e
Abed Al Ghani Mosbah Shaaban <abedshaaban600@gmail.com>
2024-05-07 23:34:13 +0300

    refactor: updated theme toggle

--- packages/create-shaaban/template-react-vite-ts-tw/src/lib/utils/theme/theme-toggle.tsx
+++ packages/create-shaaban/template-react-vite-ts-tw/src/lib/utils/theme/theme-toggle.tsx
@@ -4,19 +4,25 @@ import { Button } from '~ui/button'
 import { useTheme } from '~utils/theme/theme-provider'
 
 export function ThemeToggle() {
-  const { setTheme } = useTheme()
+  const { theme, setTheme } = useTheme()
 
   return (
-    <Button variant="outline" size="icon">
+    <Button
+      variant={'outline'}
+      size={'icon'}
+      onClick={() => setTheme(theme === 'dark' ? 'light' : 'dark')}
+    >
       <SunIcon
-        onClick={() => setTheme('dark')}
-        className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0"
+        className={
+          'h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0'
+        }
       />
       <MoonIcon
-        onClick={() => setTheme('light')}
-        className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100"
+        className={
+          'absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100'
+        }
       />
-      <span className="sr-only">Toggle theme</span>
+      <span className={'sr-only'}>Toggle theme</span>
     </Button>
   )
 }
